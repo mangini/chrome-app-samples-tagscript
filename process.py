dcd650d58f624b66a4a210dd1ff5a237dddd8872
Renato Mangini <mangini@gmail.com>
2012-09-14 22:41:14 -0700

    fixed debuging and changed github url to use https instead of git, so interactive login/pwd request can be avoided by addind a line in .netrc

--- process.py
+++ process.py
@@ -16,38 +16,39 @@ TO_EMAIL="mangini@google.com"
 #PROJECT_NAME="chrome-app-samples"
 ACCOUNT="mangini"
 PROJECT_NAME="mangini-test"
-GIT_HOME="git@github.com:%s/%s.git" % (ACCOUNT, PROJECT_NAME)
+GIT_HOME="https://github.com/%s/%s.git" % (ACCOUNT, PROJECT_NAME)
 BASE_URL="https://github.com/%s/%s" % (ACCOUNT, PROJECT_NAME)
 
-OMAHA_URL='http://omahaproxy.appspot.com/all.json'
+OMAHA_URL='http://omahaproxy.appspcot.com/all.json'
 
 
-DEBUGFILE=None
+DEBUGFILE=StringIO.StringIO()
 
-def debug(message):
+def _debug(message):
     if (DEBUG):
     	print(message)
     print >>DEBUGFILE, message
 
 def get_omaha_versions():
-	currentVersions = json.loads(urllib.urlopen(OMAHA_URL).read())
+
+	try:
+		currentVersions = json.loads(urllib.urlopen(OMAHA_URL).read())
+	except:
+		return {}
 
 	for osObj in currentVersions:
 	  if osObj['os'] == 'mac':
 	    macVersion = osObj
 	    break
 
-	if not macVersion:
-		debug("Oops, Omaha JSON does not contain a {os:'mac', ...} object: "+json.dumps(currentVersions))
-		exit(1)
-
 	versions = {}
 
-	for versionObj in macVersion['versions']: 
-		if versionObj['channel']!='canary':
-			versions[versionObj['channel']]=versionObj['version'].split('.')[0]
-	
-	debug("** Omaha has these versions: %s" % json.dumps(versions))
+	if macVersion:
+		for versionObj in macVersion['versions']: 
+			if versionObj['channel']!='canary':
+				versions[versionObj['channel']]=versionObj['version'].split('.')[0]
+		
+		_debug("** Omaha has these versions: %s" % json.dumps(versions))
 
 	return versions
 
@@ -57,16 +58,17 @@ class GitWrapper():
 		self.path=path
 		self.projectname=projectname
 		self.changes=[]
+		self.errors=[]
 
 	def execute(self, cmd, execpath=''):
-		debug("Executing: "+cmd)
+		_debug("Executing: "+cmd)
 		pr=subprocess.Popen(cmd, 
 			cwd=execpath or self.path+"/"+self.projectname, 
 			stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
 		(out, error) = pr.communicate()
-		debug("  stderr: "+error)
-		debug("  stdout: "+out)
+		_debug("  stderr: "+error)
+		_debug("  stdout: "+out)
 
 		return out
 
@@ -79,10 +81,10 @@ class GitWrapper():
 		branchinfo=self.execute('git show origin/'+branch)
 		commit_match=re.compile(r"^commit (\S+)\n").match(branchinfo)
 		if commit_match==None: 
- 	 		debug("Branch "+branch+" does not exist yet")
+ 	 		_debug("Branch "+branch+" does not exist yet")
 			return None
 		last_commit=commit_match.group(1)
-		debug("Branch "+branch+" has last commit "+last_commit)
+		_debug("Branch "+branch+" has last commit "+last_commit)
 		return last_commit
 
 	def get_remote_branches(self):
@@ -90,7 +92,7 @@ class GitWrapper():
 		self.channel_branches=re.compile(r"origin/(_[a-z]+)").findall(current_branches)
 		self.version_branches=re.compile(r"origin/(__M\d+)").findall(current_branches)
 
-		debug("** Found these remote branches: %s" % json.dumps(self.version_branches))
+		_debug("** Found these remote branches: %s" % json.dumps(self.version_branches))
 
 	def update_branches(self, version_at_channels_map):
 
@@ -99,7 +101,7 @@ class GitWrapper():
 			version = version_at_channels_map[channel]
 			br="__M"+version
 			if not br in self.version_branches:
-				debug("** Creating branch: "+br)
+				_debug("** Creating branch: "+br)
 				self.changes.append('[+version_branch] Created new version branch: %s' % self.get_branch_url(br))
 				self.create_branch(br)
 				self.version_branches.append(br)
@@ -109,13 +111,13 @@ class GitWrapper():
 			version=re.compile(r"__M(\d+)").match(branch).group(1)
 			# if version not mapped to a channel and version is not in the interval of current valid versions, remove it
 			if not version in version_at_channels_map.viewvalues() and not (branch>min(self.version_branches) and branch<max(self.version_branches)):
-				debug("** Deleting branch: "+branch)
+				_debug("** Deleting branch: "+branch)
 				self.changes.append('[-version_branch] Removed unused version branch: '+branch)
 				self.remove_branch(branch)
 				self.version_branches.remove(branch)
 
-		debug("** branches now should be: %s" % json.dumps(self.version_branches))
-		debug("** versions at channels are: %s" % json.dumps(version_at_channels_map))
+		_debug("** branches now should be: %s" % json.dumps(self.version_branches))
+		_debug("** versions at channels are: %s" % json.dumps(version_at_channels_map))
 
 		# move channel branches (stable, beta, dev) to point to the appropriate commit
 		changed=False
@@ -132,7 +134,7 @@ class GitWrapper():
 			tip_commit_channel=self.get_tip_of_branch(channel_branch)
 			tip_commit_version=self.get_tip_of_branch(version_branch)
 			if tip_commit_channel!=tip_commit_version:
-				debug("** Needs to change channel branch "+channel_branch+" from "+tip_commit_channel+" to "+tip_commit_version)
+				_debug("** Needs to change channel branch "+channel_branch+" from "+tip_commit_channel+" to "+tip_commit_version)
 				self.changes.append('[branch_move] Changed branch %s from commit %s to commit %s (version %s)' 
 					% ( self.get_branch_url(channel_branch)), tip_commit_channel, tip_commit_version, self.get_branch_url(version_branch) )
 				changed=True
@@ -143,10 +145,10 @@ class GitWrapper():
 
 
 	def get_branch_url(self, branch):
-		return '<a href="%s/tree/%s">%s</a>' % (BASE_URL, branch, branch)
+		return '%s %s/tree/%s' % (branch, BASE_URL, branch)
 
 	def update_branch_ref(self, channel_branch, commit, version):
-		debug("** Moving branch "+channel_branch+" to commit "+commit)
+		_debug("** Moving branch "+channel_branch+" to commit "+commit)
 		message='"moving channel '+channel_branch+' to version '+ version+'"'
 		self.execute('git update-ref -m '+message+' refs/heads/'+channel_branch+' '+commit)
 
@@ -173,41 +175,57 @@ def sendEmail(content, attachment):
 	msg.preamble = content
 
 	msg.attach(MIMEText(attachment, 'text'))
-	s = smtplib.SMTP('localhost')
-	s.sendmail(FROM_EMAIL, TO_EMAIL, msg.as_string())
-	s.quit()
+	try:
+		s = smtplib.SMTP('localhost')
+		s.sendmail(FROM_EMAIL, TO_EMAIL, msg.as_string())
+		s.quit()
+	except:
+		print "ERROR: could not send email: ", sys.exc_info()
+
+
+def printCollection(title, col):
+	str=""
+	if len(col)>0:
+		str+=title+"\n"
+		for item in col:
+			str+='* %s\n' % item
+	return str
 
 
 def main(argv=None):
 	versions=get_omaha_versions()
-	if not versions['stable'] or not versions['dev'] or not versions['beta']:
-		debug("Invalid Omaha input, versions missing: %s" % json.dumps(versions))
-		sys.exit(1)
+	validOmaha='stable' in versions and 'dev' in versions and 'beta' in versions
 
 	temppath = tempfile.mkdtemp()
 
-	DEBUGFILE=StringIO.StringIO()
-
 	try:
-		debug("created %s" % temppath)
+		_debug("created %s" % temppath)
 		git=GitWrapper(temppath, PROJECT_NAME)
 		
-		git.clone()
-		git.update_branches(versions)
+		if not validOmaha:
+			msg="Invalid Omaha content, versions missing: %s" % json.dumps(versions)
+			git.errors.append(msg)
+			_debug(msg)
+		else: 
+			git.clone()
+			git.update_branches(versions)
 
 	finally:
 		shutil.rmtree(temppath)
-		debug("removed %s" % temppath)
+		_debug("removed %s" % temppath)
 
-	if len(git.changes)>0:
+	if len(git.errors)>0 or len(git.changes)>0:
 		s='Branches of chrome-app-samples changed accordingly to Chrome versions. Verbose report attached, summary below:\n\n'
-		for change in git.changes:
-			s+='* %s\n' % change
+		s+=printCollection("Errors:", git.errors)
+		s+="\n"
+		s+=printCollection("Changes:", git.changes)
 		s+='\n(script run at %s)\n' % str(datetime.datetime.now())
 		sendEmail(s, DEBUGFILE.getvalue())
 	else:
-		print "Nothing changed"
+		if DEBUG: print "Nothing changed"
 
+	if len(git.errors)>0:
+		return(1)
 
 if __name__ == "__main__":
     sys.exit(main())
